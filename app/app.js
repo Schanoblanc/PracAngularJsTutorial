2840a53836c6c8b8f6a1a6f8038cf9ee893282fa
Schanoblanc <asdycu_work@126.com>
2018-12-16 18:01:05 +0800

    no message

--- app/app.js
+++ app/app.js
@@ -1,20 +1,20 @@
-'use strict'
+'use strict';
 
-//define the phonecatApp module
-var phonecatApp = angular.module('phonecatApp',[]);
+// Define the `phonecatApp` module
+var phonecatApp = angular.module('phonecatApp', []);
 
-//defien the PhoneListController contorller on the phonecatApp
+// Define the `PhoneListController` controller on the `phonecatApp` module
 phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-    $scope.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-      ];
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
 });
