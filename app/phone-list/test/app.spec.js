7e7a3bfcaf49b4a8f1a1612e8b97edf631993487
Schanoblanc <asdycu_work@126.com>
2018-12-22 21:45:47 +0800

    comment no needed unit test

--- app/phone-list/test/app.spec.js
+++ app/phone-list/test/app.spec.js
@@ -2,7 +2,7 @@ describe('PhoneListController', function() {
 
     beforeEach(module('phonecatApp'));
   
-    it('should create a `phones` model with 3 phones', inject(function($controller) {
+    xit('should create a `phones` model with 3 phones', inject(function($controller) {
       var scope = {};
       var ctrl = $controller('PhoneListController', {$scope: scope});
   
