58ba5ac90591258c9d04c0833ad52daa75b43b47
Schanoblanc <asdycu_work@126.com>
2018-12-22 21:45:31 +0800

    add $http in controller for DI

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -7,7 +7,7 @@ angular.module('phoneList').
         controller: ["$http",function PhoneListController($http) {
             var self = this;
             self.orderProp = 'age';
-            $http.get('../phones/phones.json').then(function(response){
+            $http.get('phones/phones.json').then(function(response){
                 self.phones = response.data;
             });
 
