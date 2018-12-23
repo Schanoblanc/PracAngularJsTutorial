31cc2b3dbb5dfd9cb260bc695a67b090e4081d7b
Schanoblanc <asdycu_work@126.com>
2018-12-23 15:28:01 +0800

    add detail component

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -1,9 +1,17 @@
 'use strict';
 // Register `phoneDetail` component, along with its associated controller and template
-angular.module('phoneDetail')
-.component('phoneDetail',{
-    templateUrl: 'phone-list/phone-detail.template.html',
-    controller: ['$routeParam', function phoneDetailController($routeParam){
-        this.phoneId = $routeParam.phoneId;
-    }]
-});
+angular.
+    module('phoneDetail')
+    .component('phoneDetail', {
+        templateUrl: 'phone-detail/phone-detail.template.html',
+        controller: ['$http', '$routeParams',
+            function PhoneDetailController($http, $routeParams) {
+                this.phoneId = $routeParams.phoneId;
+
+                var self = this;
+                $http.get('phones/' + this.phoneId + '.json')
+                    .then(function (response) {
+                        self.phone = response.data;
+                    })
+            }]
+    });
